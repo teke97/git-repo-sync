671908f3db3436a116cb87be3571030a0e11c9e0
teke97 <teke97@gmail.com>
2022-12-16 16:27:44 +0000

    add ssh key support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+mkdir -p ~/.ssh
+cat > ~/.ssh/config <<-EOF
+host 
+ Hostname ${INPUT_TARGET_SSH_HOST}
+ IdentityFile id_rsa
+ User git
+EOF
+
+echo "$INPUT_TARGET_SSH_KEY" > id_rsa
+chmod 600 id_rsa
+
+git remote add target ${INPUT_TARGET_URL}
 
 case "${GITHUB_EVENT_NAME}" in
     push)
